80cd8db30fb48f43449db32eb42d888954d1ba5c
SpinoPI <jixiaobo007@gmail.com>
2016-05-09 21:01:16 +0800

    The Version 0.10

--- Tunningdata.py
+++ Tunningdata.py
@@ -1,20 +1,29 @@
 # coding: utf-8
 # Code by SpinoPi
-# Version:0.9
-# test
+# Version:0.10
 # This python program is aimming to compare two tunningdata file of YOKOGAWA
 
 import pandas as pd
-lType = ['AV01', 'AV02', 'AV03', 'AV04', 'AV05', 'AV06', 'AV07', 'AV08', 'AV09',
-         'AV10', 'AV11', 'AV12', 'AV13', 'AV14', 'AV15', 'AV16', 'AV17', 'AV18',
-         'AV19', 'AV20', 'AV21', 'AV22', 'AV23', 'AV24', 'AV25', 'AV26', 'AV27',
-         'AV28', 'AV29', 'AV30', 'AV31', 'AV32', 'CIS[1]', 'CIS[2]', 'COS[1]', 'COS[2]', 'CPV',
-         'CPV1', 'CPV2', 'CPV3', 'CSV', 'DV', 'ISS', 'MV', 'NALL', 'OSS', 'P01',
-         'P02', 'P03', 'P04', 'P05', 'P06', 'P07', 'P08', 'PV', 'PVP', 'RLV1',
-         'RMV', 'RSTS', 'RSV', 'RV', 'RV01', 'RV03', 'RV05', 'RV07', 'RV09',
-         'RV1', 'RV11', 'RV13', 'RV15', 'RV17', 'RV19', 'RV2', 'RV21', 'RV23',
-         'RV25', 'RV27', 'RV29', 'RV3', 'RV31', 'RV4', 'RV5', 'RV6', 'RV7',
-         'SSTS[1]', 'SSTS[2]', 'SUM', 'SV', 'TIME']
+lType = ['AKLB[1]','AKLB[2]','AKLB[3]','AKLB[4]','AKLB[5]',
+         'AV01', 'AV02', 'AV03','AV04', 'AV05', 'AV06', 'AV07', 'AV08', 'AV09', 'AV10',
+         'AV11', 'AV12', 'AV13', 'AV14', 'AV15', 'AV16', 'AV17', 'AV18','AV19', 'AV20', 
+         'AV21', 'AV22', 'AV23', 'AV24', 'AV25', 'AV26', 'AV27','AV28', 'AV29', 'AV30',
+         'AV31', 'AV32', 
+         'CIS[1]', 'CIS[2]', 'COS[1]', 'COS[2]', 
+         'CPV', 'CPV1', 'CPV2', 'CPV3', 'CSV', 'DV', 'ISS', 'MV', 'NALL', 'OSS',
+         'P01', 'P02', 'P03', 'P04', 'P05', 'P06', 'P07', 'P08',
+         'PT01','PT02','PT03','PT04', 'PT05','PT06','PT07','PT08','PT09','PT10',
+         'PT11','PT12','PT13','PT14', 'PT15','PT16',
+         'PV', 'PVP', 'RLV1','RMV', 'RSTS', 'RSV', 'RV',
+         'RV01', 'RV03', 'RV05', 'RV07', 'RV09', 'RV1', 'RV11', 'RV13',
+         'RV15', 'RV17', 'RV19', 'RV2', 'RV21', 'RV23', 'RV25', 'RV27',
+         'RV29', 'RV3', 'RV31', 'RV4', 'RV5', 'RV6', 'RV7',
+         'SSTS[1]', 'SSTS[2]', 'SUM', 'SV',
+         'SWCR[1]','SWCR[2]','SWCR[3]','SWCR[4]','SWCR[5]',
+         'SWLB[1]','SWLB[2]','SWLB[3]','SWLB[4]','SWLB[5]',
+         'SWOP[1]','SWOP[2]','SWOP[3]','SWOP[4]','SWOP[5]',
+         'SWST[1]','SWST[2]','SWST[3]','SWST[4]','SWST[5]',
+         'TIME',]
 
 class block(object):
     def __init__(self,tag,type,value,lT):
@@ -76,21 +85,21 @@ def compareTo(dA,dB):
                 diB = {}
                 diB['Tag'] = n
                 diB['Parament'] = m
-                diB['Previous'] = dA.ix[n][m]
-                diB['New'] = dB.ix[n][m]
+                diB[dA.name] = dA.ix[n][m]
+                diB[dB.name] = dB.ix[n][m]
                 diA[i] = diB
-    return pd.DataFrame(diA,index=['Tag','Parament','Previous','New']).T
+    return pd.DataFrame(diA,index=['Tag','Parament',dA.name,dB.name]).T
 
 print('INSTRUCTION\nType "f" to get the paraments filted list.\nType "w" to get the sorted file from tunningdata.\nType "q" to quit.')
 print('=====================================')
-sA = input('The previous Tunningdata file :')
+sA = input('The previous Tunningdata file :').strip('.txt')
 if (sA == 'f' or sA == 'F'):
     print('These paraments had been filted:')
-    print(lType)
-    sA = input('The previous Tunningdata file :')
+    print(lType.sort())
+    sA = input('The previous Tunningdata file :').strip('.txt')
 elif (sA == 'w' or sA =='W'):
-    sC = input('The Tunningdata file :')
-    dC = pd.read_table(sC,skiprows=[0,1],header=None)
+    sC = input('The Tunningdata file :').strip('.txt')
+    dC = pd.read_table(sC + '.txt',skiprows=[0,1],header=None)
     dD = Tunningdata(dC,lType)
     dD.to_csv(sC.strip('.txt') + '.csv')
     print('The result had been saved into' + sC.strip('.txt') + '.csv')
@@ -99,12 +108,16 @@ elif (sA == 'w' or sA =='W'):
     quit()
 elif (sA == 'q' or sA == 'Q'):
     quit()
-sB = input('The new Tunningdata file:')
-dA = pd.read_table(sA,skiprows=[0,1],header=None)
-dB = pd.read_table(sB,skiprows=[0,1],header=None)
-dC = compareTo(Tunningdata(dA,lType),Tunningdata(dB,lType))
-sC = sA.strip('.txt') + '_' + sB.strip('.txt') + '.csv'
-dC.to_csv(sC)
+sB = input('The new Tunningdata file:').strip('.txt')
+dA = pd.read_table(sA + '.txt',skiprows=[0,1],header=None)
+dB = pd.read_table(sB + '.txt',skiprows=[0,1],header=None)
+dC = Tunningdata(dA,lType)
+dD = Tunningdata(dB,lType)
+dC.name = sA
+dD.name = sB
+dE = compareTo(dC,dD)
+sC = sA + '_' + sB + '.csv'
+dE.to_csv(sC)
 print('The result had been saved into ' + sC)
 print('Print any key to quit.')
 input()
